0a4024d6df254d5d9b1b58db6b97544a324d6ea4
Zach Perkins <thezperk@gmail.com>
2020-02-25 11:17:34 -0500

    Moved test data for db into separate file

--- where/app.py
+++ where/app.py
@@ -36,88 +36,15 @@ def index():
 <body>
     <h1>W H E R E</h1>
     <p>This is the WHERE project.</p>
-    <a href='/test_data'>Click here to nuke the database and make it all be test data.</a>
+    <a href='/test-data'>Click here to nuke the database and make it all be test data.</a>
 </body>
     """
 
 
-@app.route('/test_data')
+@app.route('/test-data')
 def test_data():
-    g.db_session.query(Point).delete()
-    g.db_session.query(Field).delete()
-    g.db_session.query(Category).delete()
-    # Water Fountain, the class.
-    wf = Category()
-    wf.name = "Water Fountain"
-    wf.icon = "https://karel.pw/water.png"
-    g.db_session.add(wf)
-    g.db_session.commit()
-    # Building
-    bd = Category()
-    bd.name = "Building"
-    bd.icon = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/basket-building-news-photo-1572015168.jpg?resize=980:*"
-    g.db_session.add(bd)
-    g.db_session.commit()
-    # Radius (Really the simplest metric we can have for building size)
-    rd = Field()
-    rd.name = "Radius"
-    rd.slug = "radius"
-    rd.type = FieldType.FLOAT
-    rd.category_id = bd.id
-    g.db_session.add(rd)
-    g.db_session.commit()
-
-    # coldness
-    cd = Field()
-    cd.name = "Coldness"
-    cd.slug = "coldness"
-    cd.type = FieldType.RATING
-    cd.category_id = wf.id
-    # filler
-    fl = Field()
-    fl.slug = "bottle_filler"
-    fl.name = "Has Bottle Filler"
-    fl.type = FieldType.BOOLEAN
-    fl.category_id = wf.id
-    g.db_session.add(cd)
-    g.db_session.add(fl)
-    g.db_session.commit()
-
-    # The johnson center
-    jc = Point()
-    jc.category = bd
-    jc.name = "Johnson Center"
-    jc.lat = 38
-    jc.lon = -77
-    jc.parent = None
-    jc.attributes = {
-        "radius": {
-            "value": 2.0
-        }
-    }
-    g.db_session.add(jc)
-
-    # A water fountain inside the JC
-    fn = Point()
-    fn.name = None
-    fn.lat = 38.829791
-    fn.lon = -77.307043
-    # fn.category_id = wf.id
-    fn.category = wf
-    fn.parent = jc
-    fn.attributes = {
-        "coldness": {
-            "num_reviews": 32,
-            "average_rating": 0.5
-        },
-        "bottle_filler": {
-            "value": True
-        }
-    }
-
-    g.db_session.add(fn)
-    g.db_session.commit()
-    return redirect('/')
+    import where.test_data
+    return redirect('/') 
 
 
 @app.route('/category/<int:id>')
